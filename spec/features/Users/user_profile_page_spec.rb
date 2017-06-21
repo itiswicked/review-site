d3acca49021166366d48f05de280a2e85a601e2e
Chris Fowke <Cfowke@Chriss-MacBook-Pro.local>
2017-06-21 12:58:28 -0500

    wip, user_profile_review_tests

--- spec/features/Users/user_profile_page_spec.rb
+++ spec/features/Users/user_profile_page_spec.rb
@@ -37,13 +37,14 @@ feature 'user interacts with profile page' do
 
   scenario 'user clicks a listed review' do
     item = FactoryGirl.create(:item, user_id: user.id)
-    review = FactoryGirl.create(:review, user_id: user.id, item_id: item.id)
+    review_user = FactoryGirl.create(:user, username: 'TestSubj', email: 'testuser@gmail.com', password: 'Changeme', avatar: 'http://lamcdn.net/hopesandfears.com/post-cover/YRI2wqllpSI_aWyAY0T11A-default.jpg')
+    review_user_item = FactoryGirl.create(:item, name: 'Vivamus elementum', description: 'Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.', user_id: review_user.id)
+    review = FactoryGirl.create(:review, user_id: user.id, item_id: review_user_item.id)
 
     sign_in user
     visit user_path(user)
+    click_link review.item.name
 
-    click_link item.name
-
-    expect(page).to have_current_path(item_path(review.item))
+    expect(page).to have_current_path(item_path(review_item))
   end
 end
